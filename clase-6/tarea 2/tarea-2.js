90b788dedb449010a1acfaf1260e5c3c0e8f6833
jguidobosi <119265701+jguidobosi@users.noreply.github.com>
2023-01-24 17:13:36 -0300

    correccion-tarea-2
    Se realizaron correcciones:
        Se modifico el programa para que los salarios sean pasados en forma de argumentos arrays a las funciones, y no buscarlos al DOM.
    Correcciones generales varias.

--- clase-6/tarea 2/tarea-2.js
+++ clase-6/tarea 2/tarea-2.js
@@ -1,97 +1,131 @@
 
-/*
-TAREA:
-Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
-Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, salario anual promedio y salario mensual promedio.
+    /*
+    TAREA:
+    Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
+    Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, salario anual promedio y salario mensual promedio.
 
-Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
-*/
-let $botonAgregar = document.querySelector("#agregar");
-let $botonQuitar = document.querySelector("#quitar");
-let $botonCalcular = document.querySelector("#calcular");
-let $divFormulario = document.querySelector("#division-formulario");
-let $resultado = document.querySelector("#resultado");
-let contadorDeFamiliares = 0;
+    Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
+    */
+    let $botonAgregar = document.querySelector("#agregar");
+    let $botonQuitar = document.querySelector("#quitar");
+    let $botonCalcular = document.querySelector("#calcular");
+    let $formularioGeneral = document.querySelector("#division-formulario");
+    let $resultado = document.querySelector("#resultado");
 
-let calcularMenor = function () {
-    let menor;
-    let flagPrimerNumero = true;
-    for (i = 0; i < contadorDeFamiliares; i++) {
+    function calcularMenor(numeros) {
+        
+        let menor = numeros[0];
 
-        let contenidoDeFormulario = document.getElementById(`input-${i + 1}`).value;
-        if (flagPrimerNumero && contenidoDeFormulario !== "") {
-            menor = Number(contenidoDeFormulario);
-            flagPrimerNumero = false;
-        } else if (menor > Number(contenidoDeFormulario) && contenidoDeFormulario !== "") {
-            menor = Number(contenidoDeFormulario);
+        for (let i = 1; i < numeros.length; i++) {
+
+           if(menor > numeros[i]){
+
+                menor = numeros[i];
+
+           }
+
+        }
+
+        return menor;
+
+    }
+    function calcularMayor(numeros) {
+        
+        let mayor = numeros[0];
+
+        for (let i = 1; i < numeros.length; i++) {
+
+           if(mayor < numeros[i]){
+
+                mayor = numeros[i];
+
+           }
+
+        }
+
+        return mayor;
+
+    }
+    function calcularPromedio(numeros) {
+        
+        let sumatoria = 0;
+        let promedio = 0;
+       
+        for (let i = 0; i < numeros.length; i++) {
+
+            sumatoria += numeros[i];
+
+        }
+
+        promedio = sumatoria/numeros.length;
+
+        return promedio;
+    }
+
+
+    $botonAgregar.onclick = function () {
+
+        $botonCalcular.disabled = false;
+        $botonQuitar.disabled = false;
+
+        let $nuevoFormulario = document.createElement("form");
+        $nuevoFormulario.className = `formulario-dinamico`;
+
+        let $nuevoInput = document.createElement("input");
+        $nuevoInput.className = `salario-anual`;
+        $nuevoInput.type = `number`;
+
+        let $nuevoLabel = document.createElement("label");
+        $nuevoLabel.innerText = `Salario anual de familiar ${document.querySelectorAll(".formulario-dinamico").length+1}:`;
+
+        $nuevoFormulario.appendChild($nuevoLabel);
+        $nuevoFormulario.appendChild($nuevoInput);
+        $formularioGeneral.appendChild($nuevoFormulario);
+
+    }
+    $botonQuitar.onclick = function () {
+
+        let nuevosFormularios = document.querySelectorAll(`.formulario-dinamico`);
+        
+        nuevosFormularios[nuevosFormularios.length-1].remove();
+        
+        if (nuevosFormularios.length === 1) {
+
+            $botonCalcular.disabled = true;
+            $botonQuitar.disabled = true;
+            $resultado.textContent = "";
+
+        };
+
+    }
+    $botonCalcular.onclick = function () {
+
+        let salariosAnuales = [];
+        let $salariosAnuales = document.querySelectorAll(".salario-anual");
+
+        for (let i = 0; i < $salariosAnuales.length; i++){
+
+            if($salariosAnuales[i].value !== ""){
+
+                 salariosAnuales.push(Number($salariosAnuales[i].value));
+                console.log(salariosAnuales);
+
+            }
+
+        }
+
+        let menorSalario = calcularMenor(salariosAnuales);
+        let mayorSalario = calcularMayor(salariosAnuales);
+        let promedioAnual = calcularPromedio(salariosAnuales);
+        let promedioMensual = (promedioAnual / 12);
+        
+        if (salariosAnuales.length === 0) {
+
+            $resultado.textContent = "No ingresaste ningun valor.";
+        
+        } else {
+
+            $resultado.textContent = `El menor salario es de $${menorSalario}, el mayor de $${mayorSalario}, el promedio anual es $${promedioAnual}, y el mensual de $${promedioMensual}.`;
+        
         }
     }
-    return menor;
-}
-let calcularMayor = function () {
-    let mayor;
-    let flagPrimerNumero = true;
-    for (i = 0; i < contadorDeFamiliares; i++) {
-        let contenidoDeFormulario = document.getElementById(`input-${i + 1}`).value;
-
-        if (flagPrimerNumero && contenidoDeFormulario !== "") {
-            mayor = Number(contenidoDeFormulario);
-            flagPrimerNumero = false;
-        } else if (mayor < Number(contenidoDeFormulario) && contenidoDeFormulario !== "") {
-            mayor = Number(contenidoDeFormulario);
-        }
-    }
-    return mayor;
-}
-let calcularPromedioAnual = function () {
-    let sumatoria = 0;
-    let promedio = 0;
-    let contadorNumerosValidos = 0;
-    for (i = 0; i < contadorDeFamiliares; i++) {
-        let contenidoDeFormulario = document.getElementById(`input-${i + 1}`).value;
-        if (contenidoDeFormulario !== "") {
-            sumatoria = sumatoria + Number(contenidoDeFormulario);
-            contadorNumerosValidos++;
-        }
-    }
-    promedio = (Math.trunc(sumatoria / contadorNumerosValidos * 100)) / 100;
-    return promedio;
-}
-
-
-$botonAgregar.onclick = function () {
-    contadorDeFamiliares++;
-    $botonCalcular.disabled = false;
-    $botonQuitar.disabled = false;
-    let $nuevoFormulario = document.createElement("form");
-    $nuevoFormulario.id = `formulario-${contadorDeFamiliares}`;
-    let $nuevoInput = document.createElement("input");
-    $nuevoInput.id = `input-${contadorDeFamiliares}`;
-    $nuevoInput.type = `number`;
-    let $nuevoLabel = document.createElement("label");
-    $nuevoLabel.innerText = `Salario anual de familiar ${contadorDeFamiliares}:`;
-
-    $nuevoFormulario.appendChild($nuevoLabel);
-    $nuevoFormulario.appendChild($nuevoInput);
-    $divFormulario.appendChild($nuevoFormulario);
-}
-$botonQuitar.onclick = function () {
-    document.getElementById(`formulario-${contadorDeFamiliares}`).remove();
-    contadorDeFamiliares--;
-    if (contadorDeFamiliares === 0) {
-        $botonCalcular.disabled = true;
-        $botonQuitar.disabled = true;
-        $resultado.textContent = "";
-    }
-}
-$botonCalcular.onclick = function () {
-    let menorSalario = calcularMenor();
-    let mayorSalario = calcularMayor();
-    let promedioAnual = calcularPromedioAnual();
-    let promedioMensual = (Math.trunc(promedioAnual / 12 * 100)) / 100;
-    if (mayorSalario === undefined) {
-        $resultado.textContent = "No ingresaste ningun valor.";
-    } else {
-        $resultado.textContent = `El menor salario es de $${menorSalario}, el mayor de $${mayorSalario}, el promedio anual es $${promedioAnual}, y el mensual de $${promedioMensual}.`;
-    }
-}
