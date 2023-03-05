ee72c555d7c67ea40971af40c25a0a6988158a13
Guido Bosi <jguidobosi@gmail.com>
2023-03-05 11:20:34 -0300

    Remove Xmas repository
    Noticed I was saving one repository inside of the other. Removed this folder to create a new repository with it.

--- js-xmas-edition-master/tarea/tarea-clase-8-2/pruebas.js
+++
@@ -1,9 +0,0 @@
-function probarValidarSalarios() {
-
-    console.assert( validarSalarios(["200000000000000000000000000"]) === `Ingrese un valor menor a ${SALARIO_MAXIMO}`, "El programa falló en la validacion de un salario mayor al maximo");
-    console.assert( validarSalarios(["29.5"]) === "No utilice puntos ni comas (redondear centavos a cantidad entera)", "El programa falló en la validacion de un numero decimal");
-    console.assert( validarSalarios(["20000000000000000000000000.5"]) === `Ingrese un valor menor a ${SALARIO_MAXIMO} y no utilice puntos ni comas (redondear centavos a cantidad entera)`, "El programa falló en la validacion de un numero de edades decimal");
-    console.assert( validarSalarios([]) === "No a ingresado ningun valor", "El programa falló en la validación de todas las entradas vacias");
-}
-
-probarValidarSalarios();
