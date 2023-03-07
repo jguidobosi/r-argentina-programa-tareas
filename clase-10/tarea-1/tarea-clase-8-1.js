eb8b14acaa31f6371f3d0e7701ce15d6f1b6a86c
Guido Bosi <jguidobosi@gmail.com>
2023-03-07 09:19:33 -0300

    Actualizacion - aprolijar codigo
    Se aprolijó el codigo dividiendo mejor las funciones y corrijiendo el código.

--- clase-10/tarea-1/tarea-clase-8-1.js
+++ clase-10/tarea-1/tarea-clase-8-1.js
@@ -5,6 +5,7 @@ necesarias.
 
 TIP: Las edades no pueden tener decimales.
 */
+
 let $botonIngresar = document.querySelector("#boton-ingresar");
 let $botonCalcular = document.querySelector("#boton-calcular");
 let $botonReiniciar = document.querySelector("#boton-reiniciar");
@@ -19,29 +20,28 @@ const MAXIMO_FAMILIARES = 25;
 
 function calcularMenor(numeros) {
 
-    let menor = numeros[0];
+    let menor = Number(numeros[0]);
 
     for (let i = 1; i < numeros.length; i++) {
 
-        if (numeros[i] < menor) {
+        if (Number(numeros[i]) < menor) {
 
-            menor = numeros[i];
+            menor = Number(numeros[i]);
 
         }
     }
 
     return menor;
 }
-
 function calcularMayor(numeros) {
 
-    let mayor = numeros[0];
+    let mayor = Number(numeros[0]);
 
     for (let i = 1; i < numeros.length; i++) {
 
-        if (numeros[i] > mayor) {
+        if (Number(numeros[i]) > mayor) {
 
-            mayor = numeros[i];
+            mayor = Number(numeros[i]);
 
         }
     }
@@ -49,7 +49,6 @@ function calcularMayor(numeros) {
     return mayor;
 
 }
-
 function calcularPromedio(numeros) {
 
     let sumatoria = 0;
@@ -57,16 +56,16 @@ function calcularPromedio(numeros) {
 
     for (let i = 0; i < numeros.length; i++) {
 
-        sumatoria += numeros[i];
+        sumatoria += Number(numeros[i]);
 
     }
 
     promedio = (sumatoria / numeros.length);
 
-    return promedio;
+
+    return (Math.trunc(promedio * 100)) / 100; //Truncar para que solo haya dos decimales.
 
 }
-
 function validarNumeroDeFamiliares(numeroDeFamiliares) {
 
     if (!/^[0-9]+$/.test(numeroDeFamiliares)) {
@@ -77,128 +76,7 @@ function validarNumeroDeFamiliares(numeroDeFamiliares) {
     }
     return "";
 }
-
-function crearEntradas(numeroDeFamiliares) {
-
-    let $seccionConFormularios = document.createElement("div");
-
-    for (let i = 0; i < numeroDeFamiliares; i++) {
-
-        let $nuevoInput = document.createElement("input");
-
-        $nuevoInput.placeholder = `Edad familiar ${i + 1}:`;
-        $nuevoInput.type = `number`;
-        $nuevoInput.id = "edad";
-        $nuevoInput.classList.add("small-top-marign");
-        $nuevoInput.classList.add("form-control");
-        
-
-        $seccionConFormularios.className = `formulario-dinamico`;
-
-        $seccionConFormularios.appendChild($nuevoInput);
-
-    }
-
-    $formularioGeneral.appendChild($seccionConFormularios);
-
-}
-
-function leerEntradas($listaDeNodosInput) {
-
-    let edadesNumeros = [];
-    let edadesStrings = [];
-
-    for (i = 0; i < $listaDeNodosInput.length; i++) {
-
-        let stringEdad = $listaDeNodosInput[i].value;
-
-        let edad = Number(stringEdad);
-
-        if (stringEdad !== "") {
-
-            edadesStrings.push(stringEdad);
-            edadesNumeros.push(edad);
-
-            if (edad >= MAXIMO_EDAD || !/^[0-9]+$/.test(stringEdad)) {
-
-                $listaDeNodosInput[i].className = "form-control is-invalid";
-
-            } else {
-
-                $listaDeNodosInput[i].className = "form-control";
-
-            }
-        }
-    }
-
-    let edades = {
-
-        strings: edadesStrings,
-        numeros: edadesNumeros
-    }
-
-    return edades;
-
-}
-
-function validarEdades(edades) {
-
-    let banderaNumeroExcesivo = false;
-    let banderaNumeroDecimal = false;
-
-    for (i = 0; i < edades.length; i++) {
-
-        if (edades[i] >= MAXIMO_EDAD) {
-            banderaNumeroExcesivo = true;
-        }
-
-        if (!/^[0-9]+$/.test(edades[i])) {
-            banderaNumeroDecimal = true;
-        }
-    }
-
-    if (banderaNumeroExcesivo && banderaNumeroDecimal) {
-        return `Ingrese las edades en numeros enteros menores a ${MAXIMO_EDAD}`;
-    }
-    if (banderaNumeroExcesivo) {
-        return `Ingrese numeros menores a ${MAXIMO_EDAD}`;
-    }
-    if (banderaNumeroDecimal) {
-        return "Ingrese numeros enteros";
-    }
-    return "";
-}
-
-function escribir(texto) {
-
-    $resultado.classList.remove("oculto");
-    if (texto === "") {
-
-        document.querySelector("#texto-resultado").remove();
-        return;
-    }
-
-    if (document.querySelector("#texto-resultado")) {
-
-        document.querySelector("#texto-resultado").innerText = texto;
-
-    } else {
-
-        let $nuevoStrong = document.createElement("strong");
-
-        $nuevoStrong.innerText = texto;
-        $nuevoStrong.id = "texto-resultado"
-        $resultado.appendChild($nuevoStrong);
-    }
-}
-
-//----------------------------------------------------
-
-$botonIngresar.onclick = function () {
-
-    let numeroDeFamiliares = $numeroDeFamiliares.value;
-
-    let errorValidacionFamiliares = validarNumeroDeFamiliares(numeroDeFamiliares);
+function manejarIngreso(errorValidacionFamiliares, numeroDeFamiliares) {
 
     if (errorValidacionFamiliares) {
 
@@ -220,15 +98,90 @@ $botonIngresar.onclick = function () {
     }
 
 }
+function crearEntradas(numeroDeFamiliares) {
 
-$botonCalcular.onclick = function () {
+    let $seccionConFormularios = document.createElement("div");
+    $seccionConFormularios.className = `formulario-dinamico`;
+
+    for (let i = 0; i < numeroDeFamiliares; i++) {
+
+        let $nuevoInput = document.createElement("input");
+
+        $nuevoInput.placeholder = `Edad familiar ${i + 1}:`;
+        $nuevoInput.type = `number`;
+        $nuevoInput.id = "edad";
+        $nuevoInput.classList.add("form-control");
+
+        let $nuevoEspacioVacio = document.createElement("div");
+        $nuevoEspacioVacio.textContent = "‎";
 
 
-    let $listaNodosInput = document.querySelectorAll("#edad");
+        $seccionConFormularios.appendChild($nuevoInput);
+        $seccionConFormularios.appendChild($nuevoEspacioVacio);
 
-    let edades = leerEntradas($listaNodosInput);
+    }
 
-    let = error = validarEdades(edades.strings);
+    $formularioGeneral.appendChild($seccionConFormularios);
+
+}
+function leerEntradas() {
+
+
+    let $listaDeNodosInput = document.querySelectorAll("#edad");
+
+    let edades = [];
+
+    for (i = 0; i < $listaDeNodosInput.length; i++) {
+
+        let edad = $listaDeNodosInput[i].value;
+
+        if (edad !== "") {
+
+            edades.push(edad);
+
+            if (Number(edad) >= MAXIMO_EDAD || !/^[0-9]+$/.test(edad)) {
+
+                $listaDeNodosInput[i].classList.add("is-invalid");
+
+            } else {
+
+                $listaDeNodosInput[i].classList.remove("is-invalid");
+
+            }
+        }
+    }
+
+    return edades;
+
+}
+function validarEdades(edades) {
+
+    let banderaNumeroExcesivo = false;
+    let banderaNumeroDecimal = false;
+
+    for (i = 0; i < edades.length; i++) {
+
+        if (Number(edades[i]) >= MAXIMO_EDAD) {
+            banderaNumeroExcesivo = true;
+        }
+
+        if (!/^[0-9]+$/.test(edades[i])) {
+            banderaNumeroDecimal = true;
+        }
+    }
+
+    if (banderaNumeroExcesivo && banderaNumeroDecimal) {
+        return `Ingrese las edades en numeros enteros menores a ${MAXIMO_EDAD}`;
+    }
+    if (banderaNumeroExcesivo) {
+        return `Ingrese numeros menores a ${MAXIMO_EDAD}`;
+    }
+    if (banderaNumeroDecimal) {
+        return "Ingrese numeros enteros";
+    }
+    return "";
+}
+function manejarIngresoEdades(error, edades) {
 
     if (error) {
 
@@ -238,20 +191,25 @@ $botonCalcular.onclick = function () {
 
         $errorEdades.textContent = "";
 
-        let edadMayor = calcularMayor(edades.numeros);
-        let edadMenor = calcularMenor(edades.numeros);
-        let edadPromedio = calcularPromedio(edades.numeros);
+        let edadMayor = calcularMayor(edades);
+        let edadMenor = calcularMenor(edades);
+        let edadPromedio = calcularPromedio(edades);
 
         let textoResultadoExitoso = `El familiar de mayor edad tiene ${edadMayor} a${ENIE}os, el menor ${edadMenor}, y tienen un promedio de ${edadPromedio} a${ENIE}os.`;
 
-        escribir(textoResultadoExitoso);
+        escribirResultado(textoResultadoExitoso);
 
     }
 
 }
+function escribirResultado(texto) {
 
-$botonReiniciar.onclick = function () {
-    
+    $resultado.classList.remove("oculto");
+
+    $resultado.innerText = texto;
+}
+function reiniciar() {
+
     $errorEdades.textContent = "";
     $errorCantidad.textContent = "";
 
@@ -268,8 +226,36 @@ $botonReiniciar.onclick = function () {
     $numeroDeFamiliares.value = "";
     $botonCalcular.classList.add("oculto");
     $botonReiniciar.classList.add("oculto");
-    
-    escribir("");
     $resultado.classList.add("oculto");
+
+}
+
+//----------------------------------------------------
+
+
+$botonIngresar.onclick = function () {
+
+    let numeroDeFamiliares = $numeroDeFamiliares.value;
+
+    let errorValidacionFamiliares = validarNumeroDeFamiliares(numeroDeFamiliares);
+
+    manejarIngreso(errorValidacionFamiliares, numeroDeFamiliares);
+
+}
+
+
+$botonCalcular.onclick = function () {
+
+    let edades = leerEntradas();
+
+    let error = validarEdades(edades);
+
+    manejarIngresoEdades(error, edades);
+
+}
+
+$botonReiniciar.onclick = function () {
+
+    reiniciar();
 
 }
